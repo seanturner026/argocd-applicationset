12d4674f33f64dd98442ce3fdfd0627756337b4e
Sean Turner <sean.turner026@gmail.com>
2024-03-11 23:24:40 -0400

    Add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# argocd-applicationset
+
+## TODO
+
+1. Is deploying `kustomization` and `helm` together possible?
+1. Test that `argo-rollouts` works with an `applicationset`
+1. Identify how to pass annotations to an `application` managed by the `applicationset`
+1. Simulate what the behavior is when switching the branch (say when wanting to upgrade).
--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,20 @@ kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manif
 
 i=0
 max_attempts=3
-until kubectl get secret argocd-initial-admin-secret -n argocd &> /dev/null; do
+secret_name=argocd-initial-admin-secret
+until kubectl get secret $secret_name -n argocd &> /dev/null
+do
   i=$((i+1))
   if [ $i -gt $max_attempts ]
   then
-    echo "Secret 'argocd-initial-admin-secret' not found after $max_attempts attempts."
+    echo "Secret '$secret_name' not found after $max_attempts attempts."
     exit 1
   fi
   sleep 1
 done
 
-password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 --decode)
+password=$(kubectl get secret $secret_name -n argocd -o jsonpath='{.data.password}' | base64 --decode)
 echo -e "\nInitial user password: $password\n"
+
+kubectl apply -f clusters/staging/argocd-app.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
