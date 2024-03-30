878366cbf3c4bcc57acf48ea5c3b7f3a468bcf13
Sean Turner <sean.turner026@gmail.com>
2024-03-30 13:22:14 -0400

    Adding revision

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/master/manif
 i=0
 max_attempts=3
 secret_name=argocd-initial-admin-secret
-until kubectl get secret $secret_name -n argocd &> /dev/null
+until kubectl get secret $secret_name -n default &> /dev/null
 do
   i=$((i+1))
   if [ $i -gt $max_attempts ]
@@ -18,8 +18,8 @@ do
   sleep 1
 done
 
-password=$(kubectl get secret $secret_name -n argocd -o jsonpath='{.data.password}' | base64 --decode)
+password=$(kubectl get secret $secret_name -n default -o jsonpath='{.data.password}' | base64 --decode)
 echo -e "\nInitial user password: $password\n"
 
 kubectl apply -f clusters/staging/argocd-app.yaml
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl port-forward svc/argocd-server -n default 8080:443
