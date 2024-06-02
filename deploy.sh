7cd4ccb4910a4255eeee434f60353b0da4f7f3ec
Sean Turner <sean.turner026@gmail.com>
2024-06-02 11:00:06 +0200

    ApplicationSet per Cluster

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 i=0
-max_attempts=3
+max_attempts=10
 secret_name=argocd-initial-admin-secret
 until kubectl get secret $secret_name -n argocd &> /dev/null
 do
   i=$((i+1))
   if [ $i -gt $max_attempts ]
   then
-    echo "Secret '$secret_name' not found after $max_attempts attempts."
+    echo "Secret '$secret_name' not found after '$max_attempts' attempts."
     exit 1
   fi
   sleep 8
@@ -21,5 +21,5 @@ done
 password=$(kubectl get secret $secret_name -n argocd -o jsonpath='{.data.password}' | base64 --decode)
 echo -e "\nInitial user password: $password\n"
 
-kubectl apply -f clusters/staging/argocd-app.yaml
+kubectl apply -f clusters/staging/argocd-appset.yaml
 kubectl port-forward svc/argocd-server -n argocd 8080:443
