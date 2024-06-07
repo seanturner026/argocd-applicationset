8f76e99dae42b80e70e11c7ffe884f5a5290b60f
Sean Turner <sean.turner026@gmail.com>
2024-06-07 19:27:01 -0400

    Adding renovate.json

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 i=0
 max_attempts=15
 secret_name=argocd-initial-admin-secret
-echo -e "\nWaiting for ArgoCD to come online..."
+echo -e "\nWaiting for ArgoCD to come online...\n"
 until kubectl get secret $secret_name -n argocd &> /dev/null
 do
   i=$((i+1))
@@ -39,6 +39,6 @@ else
 fi
 
 password=$(kubectl get secret $secret_name -n argocd -o jsonpath='{.data.password}' | base64 --decode)
-echo -e "Initial user password: $password\n"
+echo -e "\nInitial user password: $password\n"
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443
